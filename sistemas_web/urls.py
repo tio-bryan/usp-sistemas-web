f9828620498d49f0fc8dc8b8f8a8d8fd3374b688
Bryan Munekata <bryan.pesca@hotmail.com>
2019-11-25 17:32:48 -0300

    second deploy

--- sistemas_web/urls.py
+++ sistemas_web/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from drive import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('drive/', views.index),
+    path('drive/', TemplateView.as_view(template_name='drive.html'), name='home'),
+    # path('drive/', views.index),
 ]
