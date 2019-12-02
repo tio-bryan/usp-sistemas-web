9135424b70804f955d295d37759f08b895d04095
tio-bryan <bryan.pesca@hotmail.com>
2019-12-02 03:43:06 -0300

    metade da implementacao do deletar

--- drive/models.py
+++ drive/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class File(models.Model):
     name = models.CharField(max_length=500)
     filepath = models.FileField(null=True, verbose_name="")
-    owner = models.CharField(max_length=500)
+    owner = models.IntegerField()
--- drive/views.py
+++ drive/views.py
@@ -6,10 +6,14 @@ from .forms import FileForm
 
 
 def index(request):
+    all_files = File.objects.filter(owner=request.user.id)
     form = FileForm(request.POST or None, request.FILES or None)
     if form.is_valid():
-        print('teste')
+        print('teste' + str(request.user.id))
         form.save()
         return HttpResponseRedirect('/')
       
-    return render(request, 'drive.html', {'form': form})
+    return render(request, 'drive.html', {'form': form, 'all_files': all_files})
+
+def remove(id):
+    File.objects.filter(id=request.user.id)
